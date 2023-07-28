b278bb6d0a480e1f4adf5d24d5d682d9a7164125
Đàm Quốc Dân <124369155+damquocdan@users.noreply.github.com>
2023-07-28 15:35:52 +0700

    Add project files.

---
+++ Contact.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab3._02Cshap
+{
+    internal class Contact
+    {
+        //Khai báo các trường
+        public int id;
+        public string firstName;
+        public string lastName;
+        public string address;
+        public string phone;
+        public string email;
+        // tạo contructor không tham số
+        public Contact()
+        {
+
+        }
+        // tạo contructor với các tham số
+        public Contact(int id, string firstName, string lastName, string address, string phone, string email)
+        {
+            this.id = id;
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.address = address;
+            this.phone = phone;
+            this.email = email;
+        }
+        //tạo thuộc tính cho các trường
+        public int Id
+        {
+            get { return id; }
+            set { id = value; }
+        }
+        public string FirstName
+        {
+
+            get { return firstName; }
+            set { firstName = value; }
+        }
+        public string LastName
+        {
+            get { return lastName; }
+            set { lastName = value; }
+        }
+        public string Address
+        {
+            get { return address; }
+            set { address = value; }
+
+        }
+        public string Phone
+        {
+            get { return phone; }
+            set { phone = value; }
+        }
+        public string Email
+        {
+            get { return email; }
+            set { email = value; }
+        }
+        // Định nghĩa phương thức Display
+
+        public void Display()
+        {
+            Console.WriteLine("Mã số:" + id);
+            Console.WriteLine("Họ và tên:{0} {1}",firstName,lastName);
+            Console.WriteLine("Địa chỉ:" + address);
+            Console.WriteLine("Điện thoại" + phone);
+            Console.WriteLine("Email:" + email);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab3._02Cshap
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // tạo đối tượng cho contact với contructor không tham số
+            Contact ct1 = new Contact();
+            //gán giá trị cho ct1
+            ct1.Id = 1;
+            ct1.FirstName = "Đàm Quốc";
+            ct1.LastName = "Dân";
+            ct1.Address = "Từ sơn";
+            ct1.Email = "damquocdan@gmail.com";
+            ct1.Phone = "1234567890";
+
+
+            // tạo đối tượng contact với contructor có tham số
+            Contact ct2 = new Contact(2,"Trần thị","Linh","Bắc Ninh","0987654321","bupbecotinhye@hihi.com");
+            // hiển thị thông tin ct1 và ct2
+            ct1.Display();
+            ct2.Display();
+
+            Console.ReadKey();
+
+        }
+
+    }
+}
